bd299f6c005ed4383f5fa5c9689ba7e3c8358826
Ahmed Essam El Sayed <ahmedessaam124@gmail.com>
2025-01-16 20:34:06 +0200

    11111111111

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,4 +1,7 @@
   import style from "./Home.module.css" ;
+  import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSnowflake } from "@fortawesome/free-solid-svg-icons";
+import PersonalImage from "../../../public/PersonalImage/one.png"
 
   const Home = () => {
     return (
@@ -11,7 +14,31 @@
 </div>
 
 
+{/* =========================================================================================================================
+Between Two Halfs
+======================================================================================================================== */}
+
+
 <div className={style.imageHalf}>
+
+
+<div className={style.contentImge}>
+<FontAwesomeIcon className={style.oneIcon} icon={faSnowflake} />
+<FontAwesomeIcon className={style.twoIcon} icon={faSnowflake} />
+<div className={style.bgBlackImg}>
+
+<div className={style.selfImge}>
+
+<img alt="the personal image for me / Ahmed Essam El Sayed" src={PersonalImage} />
+</div>
+
+</div>
+
+<FontAwesomeIcon className={style.threeIcon} icon={faSnowflake} />
+
+<FontAwesomeIcon className={style.fourIcon} icon={faSnowflake} />
+</div>
+
 
 
 </div>
